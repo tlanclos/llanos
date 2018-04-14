17518f98ebed47868a2e3ca4e591e3d4de0afc4d
Taylor Lanclos <tlanclos@live.com>
2018-04-14 14:48:53 -0500

    Print something to test

--- os/llanos.c
+++ os/llanos.c
@@ -2,11 +2,16 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <llanos/management/memory.h>
-
-extern uint8_t* __bss_start;
-extern uint8_t* __bss_end;
+#include <llanos/video/vga.h>
 
 int kmain(void) {
-    memory_bss_clear(__bss_start, __bss_end - __bss_start);
+    vga_t vga;
+    vga_initialize(
+        &vga, 
+        vga_get_default_buffer_address(),
+         vga_get_default_terminal_width(), 
+         vga_get_default_terminal_height()
+    );
+    vga_put_string(&vga, VGA_COLOR_LIGHT_RED, VGA_COLOR_BLUE, "abc123\n");
     return 0;
 }

f892f615304fddb29ce8ea7f0bbe017a781e1d97
Taylor Lanclos <tlanclos@live.com>
2018-04-15 16:23:58 -0500

    Start creating structure for memory map fetching

--- arch/x86/memory.c
+++ arch/x86/memory.c
@@ -1,2 +1,8 @@
+#include <llanos/management/memory.h>
 #include "multiboot.h"
 
+static memory_table_t __memory_table;
+
+void get_memory_table(memory_table_t* memory_table) {
+    
+}
--- include/llanos/management/memory.h
+++ include/llanos/management/memory.h
@@ -1,4 +1,26 @@
 #pragma once
 
 #include <stdint.h>
+#include <stddef.h>
 
+#define MAX_MEMORY_TABLE_ENTRIES    32
+
+typedef struct memory_table_entry_s memory_table_entry_t;
+typedef struct memory_table_s memory_table_t;
+
+struct memory_table_entry_s {
+    uint64_t* base;
+    uint64_t length;
+};
+
+struct memory_table_s {
+    memory_table_entry_t entries[MAX_MEMORY_TABLE_ENTRIES];
+    size_t length;
+};
+
+/**
+ * Fetch a copy of the memory table mapping for resource allocation.
+ *
+ * @param memory_table pointer to where the memory table should be stored.
+ */
+extern void get_memory_table(memory_table_t* memory_table);

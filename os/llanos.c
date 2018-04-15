15d55e63422ac6de0f692cac718787e79ded4249
Taylor Lanclos <tlanclos@live.com>
2018-04-15 16:25:58 -0500

    Quick test to print some letters to the screen.
    Oh hi, LlanOS!

--- os/llanos.c
+++ os/llanos.c
@@ -6,12 +6,16 @@
 
 int kmain(void) {
     vga_t vga;
+    int i;
     vga_initialize(
         &vga, 
         vga_get_default_buffer_address(),
-         vga_get_default_terminal_width(), 
-         vga_get_default_terminal_height()
+        vga_get_default_terminal_width(), 
+        vga_get_default_terminal_height()
     );
-    vga_put_string(&vga, VGA_COLOR_LIGHT_RED, VGA_COLOR_BLUE, "abc123\n");
+    for (i = 0; i < vga.terminal_height; i++) {
+        vga_put_string(&vga, VGA_COLOR_LIGHT_RED, VGA_COLOR_BLUE, "I did NOT hit her!\n");   
+    }
+    vga_put_string(&vga, VGA_COLOR_LIGHT_RED, VGA_COLOR_BLUE, "Oh Hi, LlanOS!    \n");
     return 0;
 }

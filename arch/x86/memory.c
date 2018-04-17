b9c9bcd092070bd45752e0ecfc33dcff4b76f899
Taylor Lanclos <tlanclos@live.com>
2018-04-17 00:29:50 -0500

    Basic fetching of x86 memory table * this may be tested and re-written a bit better, but for now its a proof of concept

--- arch/x86/memory.c
+++ arch/x86/memory.c
@@ -4,5 +4,27 @@
 static memory_table_t __memory_table;
 
 void get_memory_table(memory_table_t* memory_table) {
-    
+    int entry_index;
+    multiboot_memory_map_t* memory_map = (multiboot_memory_map_t*)multiboot_info->mmap_address;
+    memory_table_entry_t* entry;
+
+    memory_table->length = 0;
+
+    for (entry_index = 0; entry_index < MAX_MEMORY_TABLE_ENTRIES; entry_index++) {
+        entry = &memory_table->entries[entry_index];
+        entry->base = 0;
+        entry->length = 0;
+    }
+
+    while (memory_map < multiboot_info->mmap_address + multiboot_info->mmap_length) {
+        entry = &memory_table->entries[memory_table->length];
+
+        if (memory_map->type == MULTIBOOT_MEMORY_AVAILABLE) {
+            entry->base = memory_map->address;
+            entry->length = memory_map->length;
+            memory_table->length++;
+        }
+
+        memory_map = (multiboot_memory_map_t*)((uint32_t)memory_map + memory_map->size + sizeof(memory_map->size));
+    }
 }
--- os/llanos.c
+++ os/llanos.c
@@ -4,18 +4,40 @@
 #include <llanos/management/memory.h>
 #include <llanos/video/vga.h>
 
+char to_hex(char b) {
+    if (b >= 0 && b <= 9) {
+        return b + '0';
+    } else if (b >= 10 && b <= 15) {
+        return b = 'a';
+    } else {
+        return '\0';
+    }
+}
+
 int kmain(void) {
+    memory_table_t memory;
     vga_t vga;
     int i;
+    int j;
+
+    get_memory_table(&memory);
+
     vga_initialize(
         &vga, 
         vga_get_default_buffer_address(),
         vga_get_default_terminal_width(), 
         vga_get_default_terminal_height()
     );
-    for (i = 0; i < vga.terminal_height; i++) {
-        vga_put_string(&vga, VGA_COLOR_LIGHT_RED, VGA_COLOR_BLUE, "I did NOT hit her!\n");   
+
+    for (i = 0; i < memory.length; i++) {
+        for (j = 7; j >= 0; j--) {
+            vga_put_character(&vga, VGA_COLOR_BLUE, VGA_COLOR_BLACK, to_hex(((uint32_t)memory.entries[i].base & (0xf << (j * 4))) >> (j * 4)));
+        }
+        vga_put_character(&vga, VGA_COLOR_BLUE, VGA_COLOR_BLACK, ' ');
+        for (j = 7; j >= 0; j--) {
+            vga_put_character(&vga, VGA_COLOR_BLUE, VGA_COLOR_BLACK, to_hex(((uint32_t)memory.entries[i].length & (0xf << (j * 4))) >> (j * 4)));
+        }
+        vga_put_character(&vga, VGA_COLOR_BLUE, VGA_COLOR_BLACK, '\n');
     }
-    vga_put_string(&vga, VGA_COLOR_LIGHT_RED, VGA_COLOR_BLUE, "Oh Hi, LlanOS!    \n");
     return 0;
 }

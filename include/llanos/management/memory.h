90be49fbdd885ad500d576d4feaaf94c08f23583
Taylor Lanclos <tlanclos@live.com>
2018-04-22 15:19:24 -0500

    Add more descriptive documentation for get_memory_table

--- include/llanos/management/memory.h
+++ include/llanos/management/memory.h
@@ -20,6 +20,9 @@ struct memory_table_s {
 /**
  * @brief Fetch a copy of the memory table mapping for resource allocation.
  *
+ * A copy of the available-memory map on the system will be fetched and if
+ * not all of the regions fit into the memory_table, they will be ignored.
+ *
  * @param memory_table pointer to where the memory table should be stored.
  */
 extern void get_memory_table(memory_table_t* memory_table);

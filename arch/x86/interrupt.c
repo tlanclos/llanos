3001bf7aca8bb5d26f80bf70e7950fc7278a1d49
Taylor Lanclos <tlanclos@live.com>
2018-04-28 14:10:52 -0500

    Define some PIC reset stuff

--- arch/x86/interrupt.c
+++ arch/x86/interrupt.c
@@ -15,8 +15,44 @@
 #define PIC2_COMMAND_PORT   0xa0
 #define PIC2_DATA_PORT      (PIC2_COMMAND_PORT + 1)
 
+/* reset PIC command for command port */
+#define PIC_RESET	0x11
+
 /* acknowledge command for command port */
 #define PIC_ACK     0x20
+
+
+/* ICW4 (not) needed */
+#define ICW1_ICW4       0x01
+
+/* Single (cascade) mode */
+#define ICW1_SINGLE     0x02
+
+/* Call address interval 4 (8) */
+#define ICW1_INTERVAL4  0x04
+
+/* Level triggered (edge) mode */
+#define ICW1_LEVEL      0x08
+
+/* Initialization - required! */
+#define ICW1_INIT       0x10
+
+
+ 
+/* 8086/88 (MCS-80/85) mode */
+#define ICW4_8086       0x01
+
+/* Auto (normal) EOI */
+#define ICW4_AUTO       0x02
+
+/* Buffered mode/slave */
+#define ICW4_BUF_SLAVE  0x08
+
+/* Buffered mode/master */
+#define ICW4_BUF_MASTER 0x0C
+
+/* Special fully nested (not) */
+#define ICW4_SFNM       0x10
 
 
 extern void __output_byte(u16 port, u8 data);
@@ -29,4 +65,33 @@ static void __pic_acknowledge(u32 interrupt_number) {
             __output_byte(PIC2_COMMAND_PORT, PIC_ACK);
         }
     }
+}
+
+static void __interrupt_init(void) {
+    /* reset both PIC controllers */
+    __output_byte(PIC1_COMMAND_PORT, PIC_RESET);
+    __output_byte(PIC2_COMMAND_PORT, PIC_RESET);
+
+    /* remap PIC1 to start at 32 */
+    __output_byte(PIC1_DATA_PORT, 32);
+
+    /* remap PIC2 to start at 40 */
+    __output_byte(PIC2_DATA_PORT, 40);
+
+    __output_byte(PIC1_DATA_PORT, ICW1_INTERVAL4);
+    __output_byte(PIC2_DATA_PORT, ICW1_SINGLE);
+
+    __output_byte(PIC1_DATA_PORT, ICW4_8086);
+    __output_byte(PIC2_DATA_PORT, ICW4_8086);
+}
+
+
+extern void load_interrupt_table(const interrupt_table_t* interrupt_table) {
+	int i;
+	interrupt_table_entry_t* entry;
+
+	for (i = 0; i < interrupt_table->length; i++) {
+		entry = &interrupt_table->entries[i];
+
+	}
 }
--- include/llanos/management/interrupts.h
+++ include/llanos/management/interrupts.h
@@ -4,15 +4,21 @@
 
 typedef struct interrupt_table_entry_s interrupt_table_entry_t;
 typedef struct interrupt_table_s interrupt_table_t;
+typedef enum interrupt_return_e interrupt_return_t;
 
 struct interrupt_table_entry_s {
     u16 hardware_interrupt_number;
-    
+    void* interrupt_arg;
+    interrupt_return_t (*interrupt_function)(u16 hardware_interrupt_number, void* interrupt_arg);
 };
 
 struct interrupt_table_s {
     interrupt_table_entry_t* entries;
     size_t length;
+};
+
+enum interrupt_return_e {
+	INTERRUPT_HANDLED = 0
 };
 
 extern void load_interrupt_table(const interrupt_table_t* interrupt_table);
